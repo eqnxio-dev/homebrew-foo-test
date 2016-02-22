83c2eb6d4ecaea4fe17ede4f71318fa0ba973315
equinox-bot-dev <bots+github@equinox.io>
2016-02-21 21:01:56 -0800

    Publish foo 0.1.6

--- Formula/foo.rb
+++ Formula/foo.rb
@@ -2,9 +2,9 @@ class Foo < Formula
   desc ''
   homepage 'http://equinox.dl:8000/foo-test/foo'
 
-  url 'http://equinox.bin:8000/a/iPUKvfSoGCn/foo-0.1.4-darwin-amd64'
-  sha256 '08d5c8a6310823c4464845c8d6c69f1ed11768b786d6f3e3020add56d3b7abce'
-  version '0.1.4'
+  url 'http://equinox.bin:8000/a/5CkDq7DYrvY/foo-0.1.6-darwin-amd64.tar.gz'
+  sha256 'c21ab03605d1c60074e1a94d8791547cf8dd682947c581f37a18b27a3a2bff07'
+  version '0.1.6'
 
   def install
     bin.install 'foo'
